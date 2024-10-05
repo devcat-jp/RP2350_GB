b30e7cb1d967b0e86b6fa74022793f61b335f8ea
devcat-jp <service@devcat.jp>
2024-10-05 22:05:57 +0900

    [add] 割り込み

---
+++ src/interrupts.hpp
@@ -0,0 +1,40 @@
+
+#ifndef INTERRUPTS
+#define INTERRUPTS
+
+
+// 割り込みで使用する定数
+const uint8_t TIMER = 1 << 2;
+const uint8_t JOYPAD = 1 << 4;
+
+
+
+class Interrupts {
+    private:
+    public:
+        bool ime;
+        uint8_t int_flags;
+        uint8_t int_enable;
+
+    // 割り込み要求
+    void irq(uint8_t val){
+        this->int_flags |= val;
+    }
+
+    inline uint8_t read(uint16_t addr){
+        if(addr == 0xFF0F) return this->int_flags;
+        if(addr == 0xFFFF) return this->int_enable;
+        return 0xFF;
+    }
+
+    inline void write(uint16_t addr, uint8_t val){
+        if(addr == 0xFF0F) this->int_flags = val;
+        if(addr == 0xFFFF) this->int_enable = val;
+    }
+
+};
+
+
+
+
+#endif
